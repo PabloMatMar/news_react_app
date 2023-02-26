d646e06cdef3731a5b68742520c4505fe303ff8e
PabMatMar <pabmatmar@gmail.com>
2023-02-26 14:41:55 +0100

    add/ Login function that saves the name in the state and logout function to eliminate it, it is rendered in all views through nav

--- src/components/Header/Nav/Nav.jsx
+++ src/components/Header/Nav/Nav.jsx
@@ -1,20 +1,29 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { userContext } from '../../../context/userContext';
 
 class Nav extends Component {
   render() {
-    return <nav>
+    return <div><nav>
       <button>
-        <Link to="/form"> addNew </Link>
+      <Link to="/form"> addNew </Link>
       </button>
       <button>
-        <Link to="/list"> News </Link>
+      <Link to="/list"> News </Link>
       </button>
       <button>
         <Link to="/"> Home </Link>
       </button>
     </nav>
-
+    <userContext.Consumer>
+        {({ logout, user }) => user.username ?
+          <span>
+          <p>Hello, {user.username}!</p><br />
+          <button variant="contained" size="small" onClick={logout}>Logout</button></span>
+          : ""
+        }
+      </userContext.Consumer>
+    </div>;
   }
 }
 
